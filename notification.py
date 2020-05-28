ef68f27328cfde272813cdc147f9af2446007201
Pratyush M <mpratyush2008@gmail.com>
2020-05-28 08:56:48 +0530

    Added notification.py

--- notification.py
+++ notification.py
@@ -3,4 +3,4 @@ from plyer import notification
 
 while True:
     notification.notify(title = 'PLEASE DRINK WATER!! :)', message = 'Regularly Drinking Water is good for Health!',app_icon = 'drink.ico', timeout = 10)
-    time.sleep(60)
+    time.sleep(60 * 60)         # Reminds every 1 hour
